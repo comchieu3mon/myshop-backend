6596b197f4c5ab4082d00fea4bd7182b169d25ae
ducminhcao <minhduc.itus@gmail.com>
2022-04-05 20:42:08 +0700

    feat: init project's skeleton

---
+++ src/main/java/com/duccao/myshopbackend/MyshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.duccao.myshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyshopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duccao/myshopbackend/MyshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duccao.myshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
