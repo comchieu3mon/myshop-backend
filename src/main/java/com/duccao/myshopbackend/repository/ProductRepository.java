3dfd819e9cc0c8973b58bd02efd99335dab388d3
ducminhcao <minhduc.itus@gmail.com>
2022-04-11 23:17:01 +0700

    feat: ProductRepository done

--- src/main/java/com/duccao/myshopbackend/repository/ProductRepository.java
+++ src/main/java/com/duccao/myshopbackend/repository/ProductRepository.java
@@ -4,7 +4,12 @@ import com.duccao.myshopbackend.domains.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {}
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+    Optional<ProductEntity> findById(UUID id);
+    List<ProductEntity> findByCategory_Id(UUID categoryId);
+}
