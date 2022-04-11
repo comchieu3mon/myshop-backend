8baed981ce66598cf860c9c18c7e6291569f6a0f
ducminhcao <minhduc.itus@gmail.com>
2022-04-11 10:41:35 +0700

    feat: added handle for binding exception

--- src/main/java/com/duccao/myshopbackend/exception/RestExceptionHandler.java
+++ src/main/java/com/duccao/myshopbackend/exception/RestExceptionHandler.java
@@ -2,15 +2,25 @@ package com.duccao.myshopbackend.exception;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(value = UserAlreadyExistException.class)
   @ResponseStatus(value = HttpStatus.CONFLICT)
   public ErrorMessage resourceNotFoundException(
@@ -28,5 +38,29 @@ public class RestExceptionHandler {
   @ResponseStatus(value = HttpStatus.BAD_REQUEST)
   public ErrorMessage illegalArgumentException(Exception exception, WebRequest webRequest) {
     return new ErrorMessage(exception.getMessage());
+  }
+
+  @Override
+  public ResponseEntity<Object> handleNoHandlerFoundException(
+          NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    return ResponseEntity.status(status)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(new ErrorMessage("Request Endpoint not found"));
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+          MethodArgumentNotValidException ex,
+          HttpHeaders headers,
+          HttpStatus status,
+          WebRequest request) {
+    List<String> errors =
+            ex.getBindingResult().getFieldErrors().stream()
+                    .map(FieldError::getDefaultMessage)
+                    .map(StringUtils::capitalize)
+                    .sorted()
+                    .toList();
+    ErrorMessage errorMessage = new ErrorMessage(String.join(". ", errors));
+    return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
   }
 }
