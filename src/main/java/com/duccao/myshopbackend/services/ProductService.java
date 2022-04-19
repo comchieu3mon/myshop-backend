fb1857a925629a06ee8757942bc4a62a28154994
duccaominh <duccaom@nashtechglobal.com>
2022-04-19 23:28:28 +0700

    feat: create product

---
+++ src/main/java/com/duccao/myshopbackend/services/ProductService.java
@@ -0,0 +1,13 @@
+package com.duccao.myshopbackend.services;
+/**
+* This is an entity class
+* This class is an implementation of interface {@link AllocationService AllocationService}
+* This class provides methods working with Allocation data
+* This class provideds UnitTest for {@Link TempService TempService}
+* The {@link AllocationController AllocationController} provides APIs for allocation logic
+* @author duccaom
+* @version 1.0
+* @since 2022/04/19
+*/
+public class ProductService {
+}
